58337cb3c22bc42039e1fddfd031d52fe5c464ea
Lee ChaeYoung <61898513+ChaeChae0505@users.noreply.github.com>
2022-07-20 09:32:54 +0900

    Create kobuki_noetic.sh

---
+++ kobuki_noetic.sh
@@ -0,0 +1,28 @@
+sudo apt install liborocos-kdl-dev
+sudo apt-get install ros-noetic-ecl-*
+sudo apt-get install libusb-dev
+sudo apt-get install libftdi-dev
+
+mkdir -p ~/kobuki_ws/src
+cd ~/kobuki_ws/src
+
+git clone https://github.com/yujinrobot/kobuki.git
+git clone https://github.com/yujinrobot/yujin_ocs.git
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone https://github.com/yujinrobot/kobuki_core.git
+
+
+cd yujin_ocs
+mkdir save 
+mv yocs_cmd_vel_mux save
+mv yocs_controllers save
+mv yocs_velocity_smoother save
+rm -rf yocs*
+cd save 
+mv * ..
+cd .. && rmdir save
+cd ~/kobuki_ws/
+catkin_make
+
+
+rosrun kobuki_ftdi create_udev_rules
